8e2feb2ca203d93bf95f415b589e268b865a7f43
Harshana Jayasekara <98016320+HarshanaJayasekara@users.noreply.github.com>
2024-03-14 16:07:49 +0530

    my dart file update
    item page, animatedButton add

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,27 @@
+import 'package:cart_page/pages/card.dart';
+import 'package:cart_page/pages/wigete.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:n_cafe/add_to_favourite_from_home_screen.dart';
-import 'package:n_cafe/screens/Login_screen.dart';
-import 'package:n_cafe/screens/Sign_up_page.dart';
-import 'package:n_cafe/screens/Verify_Email_Screen.dart';
-import 'package:n_cafe/screens/card.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowMaterialGrid: false,
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SignUpScreen(),
-        '/login': (context) => loginScreen(),
-        '/verify': (context) => Verify_Email(),
-        '/carts': (context) => CartScreen(),
-        '/itemDetails': (context) => HomePage(),
-      },
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Image.asset('assets/images/Rectangle167.png'),
-        ),
-      ),
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter Cart Example',
+      home: CartScreen(),
     );
   }
 }
---
+++ lib/pages/wigete.dart
@@ -0,0 +1,56 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Awesome Dialog Example'),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(5),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                AnimatedButton(
+                  text: 'Success Dialog',
+                  color: Colors.green,
+                  pressEvent: () {
+                    AwesomeDialog(
+                      context: context,
+                      animType: AnimType.leftSlide,
+                      headerAnimationLoop: false,
+                      dialogType: DialogType.success,
+                      showCloseIcon: true,
+                      title: 'Succes',
+                      desc:
+                          'Dialog description here..................................................',
+                      btnOkOnPress: () {
+                        debugPrint('OnClcik');
+                      },
+                      btnOkIcon: Icons.check_circle,
+                      onDismissCallback: (type) {
+                        debugPrint('Dialog Dissmiss from callback $type');
+                      },
+                    ).show();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
